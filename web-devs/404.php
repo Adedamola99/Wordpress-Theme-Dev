68164e048f5d9a667afacdb5739b52903bb35ddc
Damola <adebitandamola02@gmail.com>
2025-08-05 22:20:27 +0100

    rephrase 404 words

--- web-devs/404.php
+++ web-devs/404.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+
 <div id="content" class="site-content">
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
@@ -6,7 +7,7 @@
                 <div class="error-404">
                     <header>
                         <h1>Page not found</h1>
-                        <p>Unfortunately, the page you tried to reach does not exist on this site.</p>
+                        <p>Unfortunately, the page you are trying to reach does not exist on this site.</p>
                     </header>
 
                     <div class="error">
@@ -27,4 +28,5 @@
         </main>
     </div>
 </div>
+
 <?php get_footer(); ?>
